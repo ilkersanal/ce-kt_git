9789c3d2e58b7977718bb2e7d6558c9cfeb7c1c8
ilker sanal <heondloc@hotmail.com>
2010-12-29 12:44:47 +0100

    getter & setter methods for name created

--- src/at/jku/ce/DomainObject.java
+++ src/at/jku/ce/DomainObject.java
@@ -33,6 +33,17 @@ private String comment;
 	public String setComment(String comment){
 		this.comment = comment;
 	}	
+
+	public String getName(){
+		
+		return name;
+	}
+
+	public void setName(String name){
+		
+		this.name = name;
+	}
+
 	public String id = UUID.randomUUID().toString()
 
 }
